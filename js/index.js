20cdd0642ede4c7cbed4201e26659f636c822706
jvacdragon <joao_aguiarcosta@hotmail.com>
2022-06-07 15:31:34 -0300

    starting the api

--- js/index.js
+++ js/index.js
@@ -1,16 +1,52 @@
-console.log(`https://pokeapi.co/api/v2/pokemon/charmander`);
+
+//GET THE DATA OF THE POKEMON
+//`https://pokeapi.co/api/v2/pokemon/${namePokemon}`
+
 
 const searchBar = document.querySelector('.searchBar')
 const btnSearch = document.querySelector('.env')
 
+
+
+////GETTING THE DATA FROM DE WEB API
+const PokemonData = async (name) => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    const data = await res.json()
+
+    return data
+}
+
+
+////TRANSFORMING THE NAME AT THE SEARCH BAR
+export const lowerName = () => {
+    const name = searchBar.value
+    
+    if(!name) return;
+
+    return name.toLowerCase().trim()
+}
+
+
+////SEARCHING FOR THE POKEMON
 const searchPoke = async function searchPoke() {
     try {
-        await P.getPokemonSpecieByName("golduck")
-        const frenchName = golduckSpecies.names.filter(pokeAPIName => pokeAPIName.language.name === 'fr')[0].name
-        console.log(frenchName)
+        const name = lowerName()
+
+        const pokeData = await PokemonData(name)
+        
+        console.log(pokeData);
+        const img = `<img src = ${pokeData.sprites.front_default}>`
+
+        return document.querySelector('.card-block').insertAdjacentHTML('afterend', img)
+
     } catch (err) {
         console.error(err);
     }
 }
 
-searchPoke()
+
+
+btnSearch.addEventListener('click', function(e){
+    e.preventDefault();
+    searchPoke()
+})
